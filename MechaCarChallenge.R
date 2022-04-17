42da3be68c184b48171e31580c00d33aa9b434b0
Abdon Katter <abdonkatter@gmail.com>
2022-04-17 17:53:02 -0400

    summarize coil stats

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,3 +6,10 @@ mechacar_csv <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F
 lm_mecha <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar_csv)
 
 summary(lm_mecha)
+
+coil_csv <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
+
+total_summary <- coil_csv %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+lot_summary <- coil_csv %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
